4b8fa263356d854e5e32765c96fdc9300622c134
mRSun15 <3150105645@zju.edu.cn>
2019-05-04 04:28:10 +0800

    Update ARSC_test.sh

--- examples/ARSC_test.sh
+++ examples/ARSC_test.sh
@@ -1,14 +1,15 @@
-export GLUE_DIR="../data/glue_data"
+export GLUE_DIR="data/glue_data"
+CUDA_VISIBLE_DEVICES=1
 
 python3 run_classifier.py \
-  --task_name MRPC \
+  --task_name COLA \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --data_dir $GLUE_DIR/MRPC/ \
-  --bert_model bert-base-uncased \
+  --data_dir $GLUE_DIR/CoLA/ \
+  --bert_model bert-base-uncased-file \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 3.0 \
-  --output_dir /tmp/mrpc_output/
+  --output_dir /tmp/mrpc_output1/
