c33cb904182a33717a543773ddd470111d383a7b
mRSun15 <3150105645@zju.edu.cn>
2019-05-04 13:48:59 +0800

    Update ARSC_test.sh

--- ARSC_test.sh
+++ ARSC_test.sh
@@ -1,15 +1,14 @@
-export GLUE_DIR="data/glue_data"
-CUDA_VISIBLE_DEVICES=1
+export GLUE_DIR="data/Amazon_few_shot"
 
 python3 run_classifier.py \
-  --task_name COLA \
+  --task_name Amazon \
   --do_train \
-  --do_eval \
+  --do_eval False \
   --do_lower_case \
-  --data_dir $GLUE_DIR/CoLA/ \
+  --data_dir $GLUE_DIR \
   --bert_model bert-base-uncased-file \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 3.0 \
-  --output_dir /tmp/mrpc_output1/
+  --output_dir /tmp/Amazon_output/
