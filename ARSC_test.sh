48a9f1d0c4eeb4a9f9c68e11c8d458a2b27357b7
mRSun15 <3150105645@zju.edu.cn>
2019-05-13 12:40:38 +0800

    update ARSC_test.sh
    Update the maml version

--- ARSC_test.sh
+++ ARSC_test.sh
@@ -1,6 +1,18 @@
 export GLUE_DIR="data/Amazon_few_shot"
 
-python3 run_classifier.py \
+# python3 run_classifier.py \
+#   --task_name Amazon \
+#   --do_train \
+#   --do_eval \
+#   --do_lower_case \
+#   --data_dir $GLUE_DIR \
+#   --bert_model bert-base-uncased-file \
+#   --max_seq_length 128 \
+#   --train_batch_size 32 \
+#   --learning_rate 2e-5 \
+#   --num_train_epochs 2.0 \
+  # --output_dir /tmp/Amazon_output/
+python3 run_classifier_maml.py \
   --task_name Amazon \
   --do_train \
   --do_eval \
@@ -8,7 +20,6 @@ python3 run_classifier.py \
   --data_dir $GLUE_DIR \
   --bert_model bert-base-uncased-file \
   --max_seq_length 128 \
-  --train_batch_size 32 \
-  --learning_rate 2e-5 \
-  --num_train_epochs 2.0 \
-  --output_dir /tmp/Amazon_output/
+  --inner_learning_rate 2e-6 \
+  --outer_learning_rate 1e-5 \
+  --output_dir /tmp/Amazon_maml_output/
