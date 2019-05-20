7fe92ae90ecf178586831eb5f9e3950beaf6966e
mRSun15 <3150105645@zju.edu.cn>
2019-05-21 02:32:02 +0800

    Update ARSC_pretrain.sh

--- ARSC_pretrain.sh
+++ ARSC_pretrain.sh
@@ -1,7 +1,7 @@
 
 python3 ARSC_lm_finetuning.py \
 --train_corpus samples/sample_text.txt \
---bert_model bert-base-uncased \
+--bert_model bert-base-uncased-file \
 --do_lower_case \
 --output_dir /tmp/finetuned_lm/ \
 --train_batch_size 4 \
