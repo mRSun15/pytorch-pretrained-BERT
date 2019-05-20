b57c671ae1af58ba8b2c7e893dbd839c4ac55f21
mRSun15 <3150105645@zju.edu.cn>
2019-05-21 04:36:39 +0800

    Update generate_pretrain_file.py

--- generate_pretrain_file.py
+++ generate_pretrain_file.py
@@ -13,6 +13,7 @@ with open(os.path.join(data_dir, filter_name), 'r') as f:
     task_list = [name.strip() for name in task_list]
 out_file = open(output_file, 'w')
 task_class = ['t2', 't5', 't4']
+total_line = 0
 for task_name in task_list:
     for task in task_class:
         file_name = os.path.join(data_dir, task_name)+'.'+task+'.train'
@@ -21,7 +22,9 @@ for task_name in task_list:
             reader = csv.reader(f, delimiter="\t")
             
             for line in reader:
-                out_file.write(line[0])
+                out_file.write(line[0]+'\n')
+                total_line += 1
 
         out_file.write('\n\n')
+print(total_line)
 out_file.close()
