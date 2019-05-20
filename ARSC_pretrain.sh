fb854e370cd098b840366d17dbd7bf87e433e825
mRSun15 <3150105645@zju.edu.cn>
2019-05-21 04:26:17 +0800

    Update ARSC_pretrain.sh

--- ARSC_pretrain.sh
+++ ARSC_pretrain.sh
@@ -1,6 +1,6 @@
 
 python3 ARSC_lm_finetuning.py \
---train_corpus samples/sample_text.txt \
+--train_corpus data/Amazon_corpus.txt \
 --bert_model bert-base-uncased-file \
 --do_lower_case \
 --output_dir /tmp/finetuned_lm/ \
