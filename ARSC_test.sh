b6f4a673ad45dc6c7a5fabf29dca86bb4e966a50
mRSun15 <3150105645@zju.edu.cn>
2019-05-21 02:17:20 +0800

    Update ARSC_test.sh

--- ARSC_test.sh
+++ ARSC_test.sh
@@ -18,8 +18,8 @@ python3 run_classifier_maml.py \
   --do_eval \
   --do_lower_case \
   --data_dir $GLUE_DIR \
-  --bert_model bert-base-uncased-file \
+  --bert_model /tmp/Amazon_maml_output3/ \
   --max_seq_length 128 \
   --inner_learning_rate 2e-6 \
   --outer_learning_rate 1e-5 \
-  --output_dir /tmp/Amazon_maml_output/
+  --output_dir /tmp/Amazon_maml_output4/
