739678fe69b856530000fd451507e50d2caa8c82
mRSun15 <3150105645@zju.edu.cn>
2019-05-26 23:03:43 +0800

    Update run_classifier_maml.py

--- run_classifier_maml.py
+++ run_classifier_maml.py
@@ -1250,16 +1250,19 @@ def main():
 
             eval_loss = eval_loss / nb_eval_steps
             preds = preds[0]
-
+            softmax_output = preds
 
             if output_mode == "classification":
                 preds = np.argmax(preds, axis=1)
             elif output_mode == "regression":
                 preds = np.squeeze(preds)
-            print("Index    Prediction    Labels")
+            print("Index    Prediction    Labels    softmax_output")
             if (task_id-1)%3 == 0:
                 for i in range(len(preds)):
-                    print(i, "    ",preds[i], "    ", all_label_ids.numpy()[i])
+                    print(i, "    ",preds[i], "    ", all_label_ids.numpy()[i], softmax_output)
+                    if preds[i] != all_label_ids.numpu()[i]:
+                        print("Wrong Prediction! ")
+                        print(eval_examples[i])
             result = compute_metrics(task_name, preds, all_label_ids.numpy())
             # loss = tr_loss/nb_tr_steps if args.do_train else None
 
