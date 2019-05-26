20a2394e227bfb41a0600cccaa9490d5f61b5a1e
mRSun15 <3150105645@zju.edu.cn>
2019-05-26 12:17:33 +0800

    Update run_classifier_maml.py

--- run_classifier_maml.py
+++ run_classifier_maml.py
@@ -1247,6 +1247,11 @@ def main():
 
             eval_loss = eval_loss / nb_eval_steps
             preds = preds[0]
+            print("Prediction    Labels")
+            if (task_id-1)%3 == 0:
+                for i in range(len(preds)):
+                    print(preds[i], "    ", all_label_ids.numpy()[i])
+
             if output_mode == "classification":
                 preds = np.argmax(preds, axis=1)
             elif output_mode == "regression":
