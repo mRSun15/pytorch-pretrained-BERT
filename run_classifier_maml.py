7ed65436a020c450899b4b3a35a2aafe89b9d30f
mRSun15 <3150105645@zju.edu.cn>
2019-05-27 00:41:04 +0800

    Update run_classifier_maml.py

--- run_classifier_maml.py
+++ run_classifier_maml.py
@@ -1259,7 +1259,7 @@ def main():
             print("Index    Prediction    Labels    softmax_output")
             if (task_id-1)%3 == 0:
                 for i in range(len(preds)):
-                    print(i, "    ",preds[i], "    ", all_label_ids.numpy()[i], softmax_output)
+                    print(i, "    ",preds[i], "    ", all_label_ids.numpy()[i], softmax_output[i])
                     if preds[i] != all_label_ids.numpy()[i]:
                         print("Wrong Prediction! ")
                         print(eval_examples[i].text_a)
